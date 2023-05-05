f31d89ef75fbc2caaeb648a0eeb101aac9fe5e4c
Flash Sheridan <flash@pobox.com>
2023-05-05 18:56:11 -0400

    Start on mechanism for shared build options.

--- src/platforms/mod.rs
+++ src/platforms/mod.rs
@@ -7,3 +7,5 @@ pub mod x86_64_linux_gnu;
 pub mod x86_64_linux_musl;
 pub mod x86_64_macos;
 pub mod x86_64_windows_gnu;
+
+pub const SHARED_BUILD_OPTS: [&'static str; 3] = ["-DPACKAGE_VENDOR=\'Utter Labs\'", "-DLLVM_TARGETS_TO_BUILD=\'SyncVM\'", "-DLLVM_INCLUDE_TESTS=\'Maybe\'"]; 
