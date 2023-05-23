57214fd56d9385c0e103c79072c39b1f85c34326
Flash Sheridan <flash@pobox.com>
2023-05-23 12:23:31 -0400

    Do not merge:  verbose dry run

--- src/utils.rs
+++ src/utils.rs
@@ -8,8 +8,8 @@ use std::process::Command;
 
 use path_slash::PathBufExt;
 
-const DRY_RUN: bool = false;
-const VERBOSE: bool = false;
+const DRY_RUN: bool = true;
+const VERBOSE: bool = true;
 
 ///
 /// The subprocess runner.
