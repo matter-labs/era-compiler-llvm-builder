85515950e3306d6d97cdb0cdfa331a0902116ffa
Anton Baliasnikov <aba@matterlabs.dev>
2025-09-23 15:03:52 +0200

    chore: update llvm test branch

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -3,7 +3,7 @@ use assert_fs::fixture::FileWriteStr;
 pub const ZKSYNC_LLVM: &str = "zksync-llvm";
 pub const PACKAGE_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const ERA_LLVM_REPO_URL: &str = "https://github.com/matter-labs/era-compiler-llvm";
-pub const ERA_LLVM_REPO_TEST_BRANCH: &str = "v1.4.2";
+pub const ERA_LLVM_REPO_TEST_BRANCH: &str = "main";
 pub const ERA_LLVM_REPO_TEST_SHA_INVALID: &str = "12345abcd";
 pub const LLVM_LOCK_FILE: &str = "LLVM.lock";
 
